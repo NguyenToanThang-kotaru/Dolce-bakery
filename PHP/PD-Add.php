a1ed5866841b2a212661e00b7580b07ed8e783b7
NguyenToanThang-kotaru <NguyenToanThang15122005@gmail.com>
2025-02-05 15:12:53 +0700

    done add p1

--- PHP/PD-Add.php
+++ PHP/PD-Add.php
@@ -14,14 +14,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Kiểm tra xem file ảnh có được tải lên hay không
     if (isset($_FILES['product-image']) && $_FILES['product-image']['error'] == UPLOAD_ERR_OK) {
         $image = $_FILES['product-image']['name'];
-        $target_dir = "/Dolce-bakery/assest/PD-Manager"; // Thư mục lưu ảnh
+        $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/Dolce-bakery/assest/PD-Manager/"; // Thư mục lưu ảnh
         $target_file = $target_dir . basename($image);
 
+        echo "$target_file";
         // Tải ảnh lên
         if (move_uploaded_file($_FILES['product-image']['tmp_name'], $target_file)) {
             echo "Ảnh đã được tải lên thành công.";
+            $target_file = "/Dolce-bakery/assest/PD-Manager/".basename($image);
         } else {
             echo "Lỗi khi tải ảnh lên.";
+            echo "Mã lỗi: " . $_FILES['product-image']['error'];
             $target_file = $noneIMG; // Sử dụng ảnh mặc định khi tải ảnh không thành công
         }
     } else {
@@ -33,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $sql = "INSERT INTO products (name, type, quantity, price, image) VALUES ('$name', '$type', '$quantity', '$price', '$target_file')";
 
     if ($conn->query($sql) === TRUE) {
-        // header("Location: ../HTML/admin/admin.php");
+        header("Location: ../HTML/admin/admin.php");
         echo "Sản phẩm đã được thêm thành công!";
         exit();
 
