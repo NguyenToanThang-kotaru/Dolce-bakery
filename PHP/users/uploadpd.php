897eeed5034a017d7d37aa8c88f49ff768b1ef72
Santadura <thanhluan200510a@gmail.com>
2025-03-08 01:20:07 +0700

    php

---
+++ PHP/users/uploadpd.php
@@ -0,0 +1,36 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include '../../PHP/config.php';
+
+if (!isset($type)) {
+    die("Lỗi: Không xác định loại sản phẩm.");
+}
+
+$sql = "SELECT * FROM products WHERE type = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $type);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<div class='" . $type . "-product'>
+                <div class='product-img'>
+                    <div class='img-effect'><img src='" . $row['image'] . "' alt=''></div>
+                </div>
+                <div class='product-name'>" . $row['name'] . "</div>    
+                <div class='product-end'>
+                    <div class='price'>" . number_format($row['price'], 0, ',', '.') . " đ</div>
+                    <div class='add-cart'><img src='../../assest/cart.png' alt=''></div>
+                </div>
+              </div>";
+    }
+} else {
+    echo "<p style='text-align: center;'>Không có sản phẩm nào</p>";
+}
+
+$stmt->close();
+$conn->close();
+?>
