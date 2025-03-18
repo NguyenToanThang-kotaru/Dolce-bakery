a0b82d8e166343c11abc4ec6f3547cfad9f6257b
ALDEN <trungmai12121970@gmail.com>
2025-03-18 09:46:19 +0700

    18/3

--- PHP/config.php
+++ PHP/config.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $user = 'root'; // Tên tài khoản mặc định của XAMPP
 $password = ''; // Mật khẩu mặc định là rỗng
-$dbname = 'dolce_db';
+$dbname = 'dolce_db2';
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
