fe73e77ada31eb7f23b7b67be0b0266b63714bf0
NguyenToanThang-kotaru <NguyenToanThang15122005@gmail.com>
2025-04-12 16:49:25 +0700

    Co gang len nhe!

--- PHP/PD-getCategory.php
+++ PHP/PD-getCategory.php
@@ -1,9 +1,9 @@
 <?php
-require_once 'config.php';
+include 'config.php';
 $sql = "SELECT id, name FROM categories";
 $result = $conn->query($sql);
     echo"  <select name='product-type' class='form-select' id='product-typeFIX' required>";
-    echo"    <option value=''>-- Chọn loại sản phẩm --</option>";
+    echo"    <option value=''>-- Chọn chủng loại sản phẩm --</option>";
         if ($result && $result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 echo "<option value='{$row['id']}'>{$row['name']}</option>";
